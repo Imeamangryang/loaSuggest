b74dbae5025631b5283f353923dfa1d34802368e
신준섭 <imeamangryang@naver.com>
2024-10-08 01:26:46 +0900

    Update Part2

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color scaffoldColor = Color(0xFF252734);
+  static const Color bgLightColor = Color(0xFF333646);
+  static const Color bgLight2Color = Color(0xFF424657);
+  static const Color textFieldBgColor = Color(0xFFC8C9CE);
+  static const Color hintDarkColor = Color(0xFF666874);
+  static const Color yellowSecondaryColor = Color(0xFFFFC25C);
+  static const Color yellowPrimaryColor = Color(0xFFFFAF29);
+  static const Color whitePrimaryColor = Color(0xFFEAEAEB);
+  static const Color whiteSecondaryColor = Color(0xFFC8C9CE);
+}
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:loapetition/constants/colors.dart';
+import 'package:loapetition/constants/nav_items.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,26 +13,48 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: CustomColor.scaffoldColor,
         body: ListView(
-      scrollDirection: Axis.vertical,
-      children: [
-        // MAIN
-        Container(
-          height: 500,
-          width: double.maxFinite,
-        ),
-        // SKILL
-        Container(
-          height: 500,
-          width: double.maxFinite,
-          color: Colors.deepPurple,
-        ),
-        // PROJECTS
-        Container(
-          height: 500,
-          width: double.maxFinite,
-        ),
-      ],
-    ));
+          scrollDirection: Axis.vertical,
+          children: [
+            // MAIN
+            Container(
+              height: 60,
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: CustomColor.bgLightColor,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Row(
+                children: [
+                  const Text(
+                    '  LostArk 여론 조사',
+                    style: TextStyle(
+                      color: CustomColor.whitePrimaryColor,
+                      fontSize: 20,
+                    ),
+                  ),
+                  const Spacer(),
+                  for (var i = 0; i < navItems.length; i++)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(onPressed: () {}, child: Text(navItems[i])),
+                    ),
+                ],
+              ),
+            ),
+            // SKILL
+            Container(
+              height: 500,
+              width: double.maxFinite,
+              color: Colors.deepPurple,
+            ),
+            // PROJECTS
+            const SizedBox(
+              height: 500,
+              width: double.maxFinite,
+            ),
+          ],
+        ));
   }
 }
